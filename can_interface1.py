1386a6c8ba766f8f2cb21baea56d0359fdad8a57
Timotei Sandru <timotei.sandru2022@gmail.com>
2023-02-11 00:07:43 +0200

    extended_box_checked(), fd_box_checked() were made

--- can_interface1.py
+++ can_interface1.py
@@ -65,8 +65,8 @@ class CANInterface():
         self.can_frame6 = Frame(self.root)
         self.can_frame6.grid(row=6, column=0, sticky="nsew")
 
-        self.listbox = Listbox(self.can_frame3, yscrollcommand = 1, width = 60, selectmode=EXTENDED)
-        self.listbox1 = Listbox(self.can_frame5, yscrollcommand = 1, width = 60, selectmode =MULTIPLE)
+        self.listbox1 = Listbox(self.can_frame3, yscrollcommand = 1, width = 60, selectmode=EXTENDED)
+        self.listbox2 = Listbox(self.can_frame5, yscrollcommand = 1, width = 60, selectmode =EXTENDED)
 
         self.can_interface_Label = Label(self.can_frame1, text = "Can interface")
         self.can_interface_Label.grid(row=0, column=0, padx=20, pady=(20,0))
@@ -80,7 +80,7 @@ class CANInterface():
         self.can_interface_Label.grid(row=0, column=1, padx=20, pady=(20,0))
 
         self.drop_down_id_baudrate = OptionMenu(self.can_frame1,  self.drop_down_id_baudrate_var, *self.baudrate_list)
-        self.drop_down_id_baudrate.config(width=5)
+        self.drop_down_id_baudrate.config(width=5, state="disabled")
         self.drop_down_id_baudrate.grid(row = 1, column=1, padx= 20)
 
 
@@ -88,20 +88,20 @@ class CANInterface():
         self.can_interface_Label.grid(row=0, column=2, padx=20, pady=(20,0))
 
         self.drop_down_data_baudrate = OptionMenu(self.can_frame1, self.drop_down_data_baudrate_var, *self.data_baudrate_list)
-        self.drop_down_data_baudrate.config(width=5)
+        self.drop_down_data_baudrate.config(width=5, state="disabled")
         self.drop_down_data_baudrate.grid(row = 1, column=2, padx= 20 )
 
         
         self.fd_Label = Label(self.can_frame2, text="Fd")
         self.fd_Label.grid(row= 0, column =0, padx=(40,0), pady=(50,0))
 
-        self.fd_CkBt = Checkbutton(self.can_frame2, variable=self.fd_box)
+        self.fd_CkBt = Checkbutton(self.can_frame2, variable=self.fd_box, command= lambda: self.fd_box_checked())
         self.fd_CkBt.grid(row = 1, column=0, padx=(40,0), pady=(2,0))
 
         self.ext_flag_Label = Label(self.can_frame2, text="Ext")
         self.ext_flag_Label.grid(row =0 ,column=2, pady=(50,0))
 
-        self.ext_flag_CkBt = Checkbutton(self.can_frame2, variable=self.ext_box)
+        self.ext_flag_CkBt = Checkbutton(self.can_frame2, variable=self.ext_box, command= lambda: self.extended_box_checked())
         self.ext_flag_CkBt.grid(row=1, column=2, padx= 10, pady=(2,0))
 
 
@@ -112,10 +112,11 @@ class CANInterface():
         self.frame_id_entry.grid(row = 1, column=3, padx=(5,0), pady=(2,0))
 
 
-        self.payload_size_Label = Label(self.can_frame2, text="Payload\nSize")
+        self.payload_size_Label = Label(self.can_frame2, text="Payload\nSize", state="disabled")
         self.payload_size_Label.grid(row = 0, column=4, pady=(50,0))
 
-        self.payload_size_Entry = Entry(self.can_frame2, textvariable=self.payload_size_entry, width= 5)
+        self.payload_size_Entry = Entry(self.can_frame2, textvariable=self.payload_size_entry, width= 5, state="disabled")
+        self.payload_size_Entry.config(state="disabled", highlightbackground= "grey", highlightthickness=0)
         self.payload_size_Entry.grid(row = 1, column=4, pady=(2,0))
 
 
@@ -134,7 +135,7 @@ class CANInterface():
         self.add_to_q = Button(self.can_frame2, text="Add to q", command=self.clear_text, state="disabled", fg='red')
         self.add_to_q.grid(row = 1, column=6)
 
-        self.listbox.grid(row=0, column=0, padx=20, pady=(20,10))
+        self.listbox1.grid(row=0, column=0, padx=20, pady=(20,10))
 
         self.import_button = Button(self.can_frame4, text="Import", command = self.import_messagges)
         self.import_button.grid(row=0, column=0, padx=(20,0))
@@ -142,23 +143,23 @@ class CANInterface():
         self.save_button = Button(self.can_frame4, text="Save", command = self.import_messagges)
         self.save_button.grid(row=0, column=1, padx=10)
 
-        self.clear_button = Button(self.can_frame4, text="Clear", command = lambda: self.delete_function(self.listbox))
+        self.clear_button = Button(self.can_frame4, text="Clear", command = lambda: self.delete_function(self.listbox1))
         self.clear_button.grid(row=0, column=2)
 
-        self.listbox1.grid(row=0, column=0, padx=20, pady=(40,10))
+        self.listbox2.grid(row=0, column=0, padx=20, pady=(40,10))
 
         self.save_button = Button(self.can_frame6, text="Save", command = self.import_messagges)
         self.save_button.grid(row=0, column=1, padx=(20,10))
 
-        self.clear_button1 = Button(self.can_frame6, text="Clear", command = lambda: self.delete_function(self.listbox1))
+        self.clear_button1 = Button(self.can_frame6, text="Clear", command = lambda: self.delete_function(self.listbox2))
         self.clear_button1.grid(row=0, column=2)
 
         self.Error_label = Label(self.root, text = "")
         self.Error_label.grid(row = 1, column= 3)
         
 
-    def delete_function(self, x):
-        x.delete(ANCHOR)
+    def delete_function(self, listbox):
+        listbox.delete(ANCHOR)
 
 
     def import_messagges(self):
@@ -182,18 +183,42 @@ class CANInterface():
         print(self.string)
         self.position += 1
     
+    def fd_box_checked(self):
+        self.fd_box_checked_retVal = False
+        if self.fd_box.get() == 1:
+            self.drop_down_id_baudrate.config(state="normal")
+            self.drop_down_data_baudrate.config(state="normal")
+            self.fd_box_checked_retVal = True
+        else:
+            self.drop_down_id_baudrate.config(state="disabled")
+            self.drop_down_id_baudrate_var.set("Select")
+            self.drop_down_data_baudrate.config(state="disabled")
+            self.drop_down_data_baudrate_var.set("Select")
+            
+
+    def extended_box_checked(self):
+        self.extended_box_checked_retVal = False
+        if self.ext_box.get() == 1:
+            self.payload_size_Label.config(state="normal")
+            self.payload_size_Entry.config(state="normal")
+            self.extended_box_checked_retVal = True
+        else:
+            self.payload_size_Entry.delete(0, 'end')
+            self.payload_size_Label.config(state="disabled")
+            self.payload_size_Entry.config(state="disabled", highlightbackground= "grey", borderwidth=1)
+
 
     def check_all_fields(self):
         self.box_retVal = False
         if self.ext_box.get() == 1:
-            if int(self.frame_id_entry.get(), 16) < 255:
+            if int(self.frame_id_entry.get(), 16) < 2047:
                 self.box_retVal = True
             if len(self.frame_id_entry.get()) < 6:
                 self.box_retVal = True
         else:
             if len(self.frame_id_entry.get()) > 4:
                 self.box_retVal = True
-
+ 
 
     def clear_text(self):
         self.check_all_fields()
@@ -208,7 +233,7 @@ class CANInterface():
             self.payload_Entry.delete(0, 'end')
             self.fd_box.set(0)
             self.ext_box.set(0)
-            self.listbox.insert(self.position, self.string)
+            self.listbox1.insert(self.position, self.string)
 
 
     def progress_bar(self):
