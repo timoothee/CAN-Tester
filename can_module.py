381e7b0f4cc3130263233db85ff483a7686c9980
Timotei Sandru <timotei.sandru2022@gmail.com>
2023-03-01 06:36:33 +0200

    Delete send_q func

--- can_module.py
+++ can_module.py
@@ -9,38 +9,30 @@ class CanModule():
         self.dbaudrate = ""
         self.frame_que = []
 
-    def set_can_send_module_name(self, name):
+    #can_sender
+    def set_can_sender_module_name(self, name):
         self.can_send_module_name = name
-        print("send",self.can_send_module_name)
 
-    def get_can_send_module_name(self):
+    def get_can_sender_module_name(self):
         return self.can_send_module_name
 
-
-    def set_can_receive_module_name(self, name):
+    #can_receiver
+    def set_can_receiver_module_name(self, name):
         self.can_receive_module_name = name
-        print("r",self.can_receive_module_name)
 
-    def get_can_receive_module_name(self):
+    def get_can_receiver_module_name(self):
         return self.can_receive_module_name
 
-
+    #baudrate
     def set_baudrate(self, baudrate):
         self.baudrate = baudrate
-        print("b",self.baudrate)
 
     def get_baudrate(self):
         return self.baudrate
     
-
+    #dbaudrate
     def set_dbaudrate(self, dbaudrate):
         self.dbaudrate = dbaudrate
-        print("d",self.dbaudrate)
 
     def get_dbaudrate(self):
         return self.dbaudrate
-
-    def send_q(self):
-        for message in self.frame_que:
-            os.popen(f"cansend {self.can_send_module_name} {message}", 'w',1)
-            print(f"cansend {self.can_send_module_name} {message}")
