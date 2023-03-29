4be648a7fb12707a42baacd4bf91f88a77ed77c0
Timotei Sandru <timotei.sandru2022@gmail.com>
2023-03-29 06:09:30 +0300

    Change file structure

--- main.py
+++ main.py
@@ -4,13 +4,23 @@ from can_module import CanModule
 from tkinter import ttk
 import os
 
-gui = CANGui("v.1.8.0")
-# MAJOR.MINOR.PATCH
+
 
 if os.environ.get('DISPLAY','') == '':
     print('no display found. Using :0.0')
     os.environ.__setitem__('DISPLAY', ':0.0')
 
 if __name__ == "__main__":
+    root = Tk()
+    splash = SplashScreen(root)
+    for i in range(100):
+        root.update()
+        splash.progressbar.step(1)
+        time.sleep(0.01)
+    splash.destroy()
+    root.mainloop()
+    
+    gui = CANGui("v.1.8.0")
+    # MAJOR.MINOR.PATCH
     gui.build()
     gui.root.mainloop()
