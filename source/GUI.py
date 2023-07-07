3d078114ffae2da264d130119a30274f03eed1b2
Timoothee <timotei.sandru2022@gmail.com>
2023-07-07 08:00:18 +0300

    CHanges

--- source/GUI.py
+++ source/GUI.py
@@ -815,6 +815,8 @@ class CANGui():
                 for i in range(len(self.list_mem) ,len(self.list_read)):
                     self.list_read[i] = self.list_read[i].replace(b'\x00'.decode(),'')
                     self.list_read[i] = self.list_read[i].replace(b'\n'.decode(),'')
+                    print("---", self.list_read)
+                    print("++",self.list_read[i].strip())
                     self.can_bus_listbox.insert('end', self.list_read[i])
                     self.can_bus_listbox.see(END) 
                 self.list_mem = self.list_read
