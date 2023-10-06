3bf783df8cfc8385fae14f307f7d505adad2389a
Timoothee <timotei.sandru2022@gmail.com>
2023-10-06 18:17:56 +0300

    Add lib file
    Here you can store all your imports,
    then in your main file you import only lib.

--- source/GUI.py
+++ source/GUI.py
@@ -1,26 +1,4 @@
-from tkinter import *
-from tkinter.ttk import Progressbar
-import time
-from tkinter import ttk
-import ast
-import os
-from tkinter import messagebox
-import sys
-#from tkmacosx import Button
-import can_module as CAN_module
-import can_frame as CAN_frame
-import test_canbus as Can_Test
-import fade
-import psutil
-import platform
-import threading
-from tkinter.filedialog import asksaveasfile
-import random
-from PIL import Image, ImageTk
-import subprocess
-import webbrowser
-from collections import Counter
-from tkinter import font
+from lib import *
 
 class CANGui():
     def __init__(self, gui_revision: str):
